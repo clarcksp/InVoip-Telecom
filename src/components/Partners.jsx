8b20a7a6a9cddcb6e7f1e3b381dec986a5fb147d
clarcksp <ronaldosc86@gmail.com>
2025-06-05 16:55:46 -0300

    Update

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -8,12 +8,14 @@ const Partners = () => {
         <h2 className="text-3xl font-bold text-center mb-12">Parceiros Oficiais</h2>
         <div className="flex flex-wrap justify-center items-center gap-8">
           <PartnerLogo
-            src="https://www.yeastar.com/wp-content/uploads/2022/10/yeastar-logo.png"
+            src="https://www.inside.gru.br/images/Yeastar_logo.png"
             alt="Yeastar"
+            className="h-12"
           />
           <PartnerLogo
-            src="https://www.3cx.com/wp-content/themes/3cx/img/3cx-logo.png"
+            src="https://www.inside.gru.br/images/3CX_logo.png"
             alt="3CX"
+            className="h-12"
           />
         </div>
       </div>
